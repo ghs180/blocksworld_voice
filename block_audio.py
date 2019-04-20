7dc21bf6b66a3dba75e22a0f5e07ecac1f407e19
George Hatzis-Schoch <ghs180@gmail.com>
2019-04-20 16:58:28 -0400

    Better voice control input

--- block_audio.py
+++ block_audio.py
@@ -6,6 +6,8 @@ import math
 from os import system
 import threading
 import subprocess
+import time
+
 
 # Microphone stream config.
 CHUNK = 1024  # CHUNKS of bytes to read each time from mic
@@ -19,21 +21,21 @@ SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
                    # only silence is recorded. When this time passes the
                    # recording finishes and the file is delivered.
 
-
-textfile = """time_step 0.005\nxyz 0.5 -0.5 0.5\nhpr 90.0 -25.0 0.0\n"""
-
+#Init text file
+textfile_init = """time_step 0.005\nxyz 0.5 -0.5 0.5\nhpr 90.0 -25.0 0.0\n"""
+textfile = textfile_init
 file1 = open("test.txt" , "w")
 file1.truncate(0)
 file1.write(textfile)
+file1.close()
 
+'''
 def audio_int(num_samples=50):
     """ Gets average audio intensity of your mic sound. You can use it to get
         average intensities while you're talking and/or silent. The average
         is the avg of the 20% largest intensities recorded.
     """
-
     p = pyaudio.PyAudio()
-
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
@@ -47,68 +49,56 @@ def audio_int(num_samples=50):
     print(" Average audio intensity is ", r)
     stream.close()
     p.terminate()
-
     if r > THRESHOLD:
-        listen(0)
-
+        listen()
     threading.Timer(SILENCE_LIMIT, audio_int).start()
+'''
 
 
-"""
-def listen(x):
+
+def listen(file1):
     r=rs.Recognizer()
-    if x == 0:
-        system('say Hi. How can I help?')
-    with rs.Microphone() as source:
-        audio=r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        y = process(text.lower())
-        return(y)
-    except:
-        if x == 1:
-            system('say Good Bye!')
-        else:
-            system('say I did not get that. Please say again.')
-            listen(1)
-"""
+    while(True):
+        system('say Please state your command.')
+        with rs.Microphone() as source:
+            audio=r.listen(source)
+            try:
+                text = r.recognize_google(audio)
+            except:
+                system('say Please repeat your command.')
+                text = r.recognize_google(audio)
+            res = process(text.lower(), file1)
+            return res
 
-def listen(x, p):
-    r=rs.Recognizer()
-    if x == 0:
-        system('say Hi. How can I help?')
-    with rs.Microphone() as source:
-        audio=r.listen(source)
-        text = r.recognize_google(audio)
-        res = process(text.lower(), p)
-        return res
-
-def process(text, p):
+def process(text, file1):
  #   ''''''''''''''''''''''''''''''''''''''''''''''''
  #   '''''''Your application goes here ''''''''''''''
  #   ''''''''''''''''''''''''''''''''''''''''''''''''
+    global textfile
     words = text.split(' ')
     print(words)
     if words[0] == "exit":
         return -1
-    if words[0] == "place":
+    if words[0] == "simulate" or words[0] == "run":
+        return 2
+    if words[0] == "clear" or words[0] == "wipe":
+        return -2
+    if words[0] == "wait":
+        return -3
+    if words[0] == "pause":
+        return (-3, int(words[1]))
+    if words[0] == "build":
         if words[1] == "block":
             if words[2] == "left":
-                #file1.truncate(0)
                 write_str = "block 0.1 0.05285 0.00385 0 0 0\n"
-                file1.write(write_str)
-                file1.close()
-                process = subprocess.run(['./blocks-mac','test.txt'])
+                textfile += write_str
+                return 1
             elif words[2] == "right":
-                write_str = "block 0.9 0.1549 0.05875 0 1.5708 1.5708 \n"
-                file1.write(write_str)
-                file1.close()
-                process = subprocess.run(['./blocks-mac','test.txt'])
-
-    if words[0] == "build":
-        if words[1] == "structure":
+                write_str = "block 0.9 0.1549 0.05875 0 1.5708 1.5708\n"
+                textfile += write_str
+                return 1
+        elif words[1] == "structure":
             if words[2] == "right":
-                #file1.truncate(0)
                 write_str = """block 0.9 0.0451 0.05875 0 1.5708 1.5708\n
                                block 0.9 0.1549 0.05875 0 1.5708 1.5708\n
                                block 0.9 0.1 0.12135 0 0 1.5708\n
@@ -120,10 +110,9 @@ def process(text, p):
                                block 0.9 0.1 0.37175 0 0 1.5708\n
                                block 0.9 0.0451 0.43435 0 1.5708 1.5708\n
                                block 0.9 0.1549 0.43435 0 1.5708 1.5708\n
-                               block 0.9 0.1 0.49695 0 0 1.5708"""
-                file1.write(write_str)
-                file1.close()
-                process = subprocess.run(['./blocks-mac','test.txt'])
+                               block 0.9 0.1 0.49695 0 0 1.5708\n"""
+                textfile += write_str
+                return 1
             elif words[2] == "left":
                 write_str = """block 0.1 0.05285 0.00385 0 0 0\n
                                block 0.1 0.14715 0.00385 0 0 0\n
@@ -140,13 +129,9 @@ def process(text, p):
                                block 0.1 0.05285 0.05005 0 0 0\n
                                block 0.1 0.14715 0.05005 0 0 0\n
                                block 0.05285 0.1 0.05775 0 0 1.5708\n
-                               block 0.14715 0.1 0.05775 0 0 1.5708"""
-                file1.write(write_str)
-                file1.close()
-                process = subprocess.run(['./blocks-mac','test.txt'])
-
-
-
+                               block 0.14715 0.1 0.05775 0 0 1.5708\n"""
+                textfile += write_str
+                return 1
     return 0
 
 
@@ -154,7 +139,47 @@ def process(text, p):
 
 #process = subprocess.run(['./blocks-mac','test.txt'])
 
+'''
+Steps:
+0. Run simulation initially empty
+1. Ask user if they would like to add object(s) to grid
+2. User adds object to grid using voice/remote
+3. Run simulation and repeat step 1
+ 
+'''
+
+#Step 0
+
+
+#Run simulation with empty textfile
+sprocess = subprocess.Popen(['./blocks','test.txt'])
+
+
 while True:
-    res = listen(0, process)
+    res = listen(file1)
     if res == -1:
         break
+    if res == -2:
+        system('say OK, here is a fresh slate.')
+        textfile = textfile_init
+    if res == -3:
+        system('say Good night!')
+        time.sleep(10)
+    if type(res) == tuple:
+        system('say Good night!')
+        x = res[1]
+        time.sleep(x)
+
+
+    if res == 0:
+        system('say Unable to recognize your command.')
+
+    if res == 2:
+        sprocess.kill()
+        file1 = open("test.txt" , "w")
+        file1.truncate(0)
+        file1.write(textfile)
+        file1.close()
+        sprocess = subprocess.Popen(['./blocks','test.txt'])
+        time.sleep(10)
+        
